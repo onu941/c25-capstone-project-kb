4bbcfb19067e3b670ac96cae5142ad35edf89f0e
John Doe <123931543+onu941@users.noreply.github.com>
2023-05-29 17:14:56 +0800

    testing tailwind

--- partyroom/src/App.tsx
+++ partyroom/src/App.tsx
@@ -1,15 +1,28 @@
-import { Center, Container, Flex, MantineProvider, Text } from "@mantine/core";
+import { useState } from "react";
+import React from "react";
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <MantineProvider
-      theme={{ colorScheme: "dark" }}
-      withGlobalStyles
-      withNormalizeCSS
-    >
-      <Container fluid style={{ height: "100%" }}>
-        <Center>Text</Center>
-      </Container>
-    </MantineProvider>
+    <>
+      <div className="bg-white dark:bg-slate-800 h-screen text-white flex flex-col place-content-center place-items-center">
+        <div className="h-36 w-52 dark:bg-slate-500 flex place-content-center place-items-center mb-12 rounded-lg">
+          Logo
+        </div>
+        <button className="dark: bg-slate-500 py-2 px-4 mb-6 rounded-2xl text-white">
+          New User
+        </button>
+        <button
+          className="dark: bg-slate-500 py-2 px-4 mb-6 rounded-2xl text-white"
+          onClick={() => setCount(count + 1)}
+        >
+          Existing User
+        </button>
+        {/* <div>count: {count}</div> */}
+      </div>
+    </>
   );
 }
+
+export default App;
