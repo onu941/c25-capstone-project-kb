3d0b63aa8f5dfa33cca43df8837c8e486092566a
John Doe <123931543+onu941@users.noreply.github.com>
2023-06-03 16:47:54 +0800

    minor edit

--- nest-server/src/app.controller.ts
+++ nest-server/src/app.controller.ts
@@ -1,13 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { InjectKnex, Knex } from 'nestjs-knex';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    @InjectKnex() private readonly knex: Knex,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
