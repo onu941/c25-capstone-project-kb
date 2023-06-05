e21cc40224bfb04912542d6fc227b2420b59f328
John Doe <123931543+onu941@users.noreply.github.com>
2023-06-05 20:19:14 +0800

    update NewRoom

--- vite-client/src/components/Part2Form.tsx
+++ vite-client/src/components/Part2Form.tsx
@@ -8,8 +8,8 @@ export interface Form2Props {
   getRootProps: <T extends DropzoneRootProps>(props?: T | undefined) => T;
   getInputProps: <T extends DropzoneInputProps>(props?: T | undefined) => T;
   isDragActive?: boolean;
-  enabled?: boolean;
-  setEnabled: React.Dispatch<React.SetStateAction<boolean>>;
+  switchEnabled?: boolean;
+  setSwitchEnabled: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export function Part2Form(props: Form2Props) {
@@ -31,21 +31,24 @@ export function Part2Form(props: Form2Props) {
       </div>
       <p className="text-center mb-8 text-xl">Image Preview here</p>
       <FormCarousel />
+
       <div className="columns-2 flex place-content-center place-items-center gap-5 mb-8">
         <div>
           <p className="text-slate-300 text-md">I'm done uploading!</p>
         </div>
         <div>
           <Switch
-            checked={props.enabled}
-            onChange={props.setEnabled}
-            className={`${props.enabled ? "bg-pink-500" : "bg-pink-800"}
+            checked={props.switchEnabled}
+            onChange={props.setSwitchEnabled}
+            className={`${props.switchEnabled ? "bg-pink-500" : "bg-pink-800"}
         relative inline-flex h-[29px] w-[52px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors shadow-lg duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
           >
             <span className="sr-only">Use setting</span>
             <span
               aria-hidden="true"
-              className={`${props.enabled ? "translate-x-6" : "translate-x-0"}
+              className={`${
+                props.switchEnabled ? "translate-x-6" : "translate-x-0"
+              }
           pointer-events-none inline-block h-[24px] w-[24px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
             />
           </Switch>
