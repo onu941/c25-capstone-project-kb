0849198374d2b865694ca68611299ff7006d517a
John Doe <123931543+onu941@users.noreply.github.com>
2023-06-07 15:53:36 +0800

    signup hashed password route

--- nest-server/src/user/user.service.ts
+++ nest-server/src/user/user.service.ts
@@ -1,12 +1,56 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { User } from './user.interface';
 import { InjectKnex } from 'nestjs-knex';
 import { Knex } from 'knex';
+import { CreateUserDto } from 'src/partyroom/dto/create-user.dto';
+import { checkPassword, hashPassword } from './hash';
 
 @Injectable()
 export class UserService {
-  users: User[];
+  private users: User[];
   constructor(@InjectKnex() private readonly knex: Knex) {}
+
+  async createUser(CreateUserDto: CreateUserDto) {
+    try {
+      console.log('userService CreateUserDto:', CreateUserDto);
+      const { name, email, phone, password, image_id, is_admin } =
+        CreateUserDto;
+      const hashedPassword = await hashPassword(password);
+      const [id] = await this.knex('users')
+        .insert({
+          name,
+          email,
+          phone,
+          password: hashedPassword,
+          image_id,
+          is_admin,
+        })
+        .returning(['id', 'name']);
+      return { id };
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.knex
+      .table('users')
+      .where({ email })
+      .first(['id', 'name', 'email', 'password']);
+    if (!user) {
+      return null;
+    }
+    const isPasswordMatch = await checkPassword(password, user.password);
+    if (!isPasswordMatch) {
+      return null;
+    }
+    const { password: _, ...result } = user;
+    return result;
+  }
 
   async getUserList() {
     const users = await this.knex
