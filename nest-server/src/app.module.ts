fcafaca79d901899ac1c5d58ddd246718902dfa3
John Doe <123931543+onu941@users.noreply.github.com>
2023-06-08 13:48:47 +0800

    Revert "Revert "testing authroutes""
    This reverts commit 49f75657956b0236b3ccab1ca8768205d3667ce5.

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PartyroomModule } from './partyroom/partyroom.module';
 import { KnexModule } from 'nestjs-knex';
+import { AuthModule } from './auth/auth.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 @Module({
-  imports: [UserModule, PartyroomModule],
+  imports: [UserModule, PartyroomModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
