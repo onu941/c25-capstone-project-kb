444187f9fa3da462e9c45586a6cd4d996a5ccd1f
John Doe <123931543+onu941@users.noreply.github.com>
2023-06-09 12:04:38 +0800

    clean up console logs

--- vite-client/src/redux/authAPI.ts
+++ vite-client/src/redux/authAPI.ts
@@ -1,10 +1,4 @@
 export async function localLogin(email: string, password: string) {
-  console.log(
-    "localLogin called with email:",
-    email,
-    "and password:",
-    password
-  );
   const res = await fetch(`http://localhost:3000/user/login`, {
     // need help setting process.env
     method: "POST",
@@ -21,10 +15,8 @@ export async function localLogin(email: string, password: string) {
   if (res.status === 201 || res.status === 200) {
     // why 201 and not 200
     localStorage.setItem("token", result.token);
-    console.log("localLogin success: token stored in localStorage");
     return true;
   } else {
-    console.log("localLogin failed with status:", res.status);
     return false;
   }
 }
