cf1f1dd0cd1bb5532f037c3ba477caf920970965
John Doe <123931543+onu941@users.noreply.github.com>
2023-06-12 18:05:51 +0800

    setting up knex seed

---
+++ nest-server/seeds/00-reset-db.ts
@@ -0,0 +1,22 @@
+import { Knex } from 'knex';
+
+export async function seed(knex: Knex): Promise<void> {
+  const tables = [
+    'review',
+    'chat',
+    'booking_info',
+    'partyroom_price_list',
+    'partyroom_equipment',
+    'partyroom_category',
+    'partyroom_image',
+    'equipment',
+    'category',
+    'partyroom',
+    'users',
+    'district',
+    'image',
+  ];
+  for (const table of tables) {
+    await knex.raw(`TRUNCATE ${table} RESTART IDENTITY CASCADE;`);
+  }
+}
