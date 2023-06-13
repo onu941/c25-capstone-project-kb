6c7e6e04574bef873765a9bbce3b4ed465372d1b
John Doe <123931543+onu941@users.noreply.github.com>
2023-06-13 14:30:49 +0800

    updates

--- vite-client/src/components/SetRooms.tsx
+++ vite-client/src/components/SetRooms.tsx
@@ -1,13 +1,65 @@
-import { Link } from "react-router-dom";
-import { PartyroomCard } from "./minicomponents/Cards";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { BookingCard, PartyroomCard } from "./minicomponents/Cards";
 import { FullScreen } from "./minicomponents/Containers";
+import { useEffect, useState } from "react";
+
+export interface PartyroomInSettings {
+  id: number;
+  name: string;
+  host_id: number;
+  address: string;
+  is_hidden: boolean;
+}
 
 export function SetRooms() {
+  const searchParams = new URLSearchParams(location.search);
+  const userId = searchParams.get("user_id");
+  const navigate = useNavigate();
+
+  const [userPartyrooms, setUserPartyrooms] = useState<PartyroomInSettings[]>(
+    []
+  );
+
+  useEffect(() => {
+    const fetchUserPartyrooms = async () => {
+      const token = localStorage.getItem("token");
+      const params = new URLSearchParams(window.location.search);
+      const userId = params.get("user_id");
+      const response = await fetch(
+        `http://localhost:3000/partyroom/user/${userId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const partyroomsData = await response.json();
+      setUserPartyrooms(partyroomsData);
+    };
+
+    fetchUserPartyrooms();
+  }, []);
+
   return (
-    <FullScreen>
-      <Link to="/partyroom">
-        <PartyroomCard name="Partyroom Name" address="Address" />
-      </Link>
-    </FullScreen>
+    <div className="flex flex-row w-full pt-10 place-content-center">
+      <div className="grid grid-cols-3 gap-8 w-fit">
+        {userPartyrooms.map((partyroom) => (
+          <div className="mx-4">
+            <PartyroomCard
+              id={partyroom.id}
+              name={partyroom.name}
+              address={partyroom.address}
+              onClick={() => {
+                navigate(
+                  `/partyroom?user_id=${userId}&partyroom_id=${partyroom.id}`
+                );
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
--- vite-client/src/components/minicomponents/Cards.tsx
+++ vite-client/src/components/minicomponents/Cards.tsx
@@ -5,6 +5,7 @@ import {
 } from "@heroicons/react/20/solid";
 
 interface CardProps {
+  id?: number;
   date?: number;
   month?: string;
   year?: number;
@@ -14,13 +15,15 @@ interface CardProps {
   address?: string;
   content?: string;
   score?: number;
+  onClick?: () => void;
+  image?: string;
 }
 
 export function BookingCard(props: CardProps) {
   return (
     <div className="flex place-content-center">
-      <div className="bg-slate-500 md:w-96 md:px-4 px-6 md:py-7 py-5 mb-6 rounded-md columns-2 flex drop-shadow-lg outline outline-offset-2 outline-slate-400 gap-4">
-        <div className="w-full flex place-content-center columns-2">
+      <div className="bg-slate-800 md:w-96 md:px-4 px-6 md:py-7 py-5 mb-6 rounded-xl columns-2 flex drop-shadow-lg border-solid border-2 border-slate-400 gap-4">
+        <div className="w-full flex place-content-center columns-2 text-slate-100">
           <div className="w-fit flex place-content-end place-items-center pr-2 md:text-7xl text-6xl">
             {props.date}
           </div>
@@ -30,13 +33,13 @@ export function BookingCard(props: CardProps) {
           </div>
         </div>
         <div className="w-full flex flex-col place-content-center place-items-center">
-          <div className="font-semibold md:text-lg text-base mb-2 text-center">
+          <div className="font-semibold md:text-lg text-base mb-2 text-center text-slate-100">
             {props.name}
           </div>
-          <div className="md:text-base text-base mb-1">
+          <div className="md:text-base text-base mb-1 text-slate-300">
             {props.time} | {props.pax} pax
           </div>
-          <div className="md:text-base text-base text-center">
+          <div className="md:text-base text-base text-center text-slate-300">
             {props.address}
           </div>
         </div>
@@ -45,13 +48,40 @@ export function BookingCard(props: CardProps) {
   );
 }
 
+export function BookingCardLarge(props: CardProps) {
+  return (
+    <div className="card lg:card-side bg-base-100 shadow-xl">
+      <figure>
+        <img src={props.image} alt="Album" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{props.name}</h2>
+        <p>{props.address}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">See more</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export function PartyroomCard(props: CardProps) {
   return (
-    <div className="flex place-content-center">
-      <div className="bg-slate-500 w-11/12 place-content-center place-items-center px-4 py-7 mb-6 rounded-md columns-2 flex flex-col drop-shadow-lg outline outline-offset-2 outline-slate-400">
-        <div className="text-3xl mb-5 font-semibold">{props.name}</div>
-        <div className="text-xl text-slate-300">{props.address}</div>
-      </div>
+    <div className="flex place-content-center mb-8" data-id={props.id}>
+      <a href="" onClick={props.onClick} className="w-full flex justify-center">
+        <div className="bg-slate-800 md:w-96 md:px-4 px-6 md:py-7 py-5 rounded-xl columns-2 flex drop-shadow-lg border-solid border-2 border-slate-400 gap-4">
+          <div className="w-full h-full">
+            <div className="w-full flex flex-col place-content-center place-items-center">
+              <div className="font-semibold md:text-2xl text-base mb-7 text-center">
+                {props.name}
+              </div>
+              <div className="md:text-lg text-base text-center text-slate-300">
+                {props.address}
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
     </div>
   );
 }
