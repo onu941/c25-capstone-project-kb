84e246f0be5e5b11379078bb37f5f3abaf90190d
John Doe <123931543+onu941@users.noreply.github.com>
2023-06-15 16:01:32 +0800

    fix typo

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -9,12 +9,11 @@ import { JwtModule } from '@nestjs/jwt';
 import * as dotenv from 'dotenv';
 import { JwtStrategy } from './auth/jwt.strategy';
 import { UserService } from './user/user.service';
-import { BookingsModule } from './bookings/bookings.module';
 import { BookingModule } from './booking/booking.module';
 dotenv.config();
 
 @Module({
-  imports: [UserModule, PartyroomModule, AuthModule, BookingsModule, BookingModule],
+  imports: [UserModule, PartyroomModule, AuthModule, BookingModule],
   controllers: [AppController],
   providers: [AppService, JwtStrategy, UserService],
 })
