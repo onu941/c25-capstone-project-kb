26ffe1d4f8d8d689d67d99ef95e9c6c208cf00df
chkccing <cliffordlkyhk@gmail.com>
2023-06-16 01:19:51 +0000

    add new migration - 02function

--- data-warehouse/knex-migration/seeds/01-chance.ts
+++ data-warehouse/knex-migration/seeds/01-chance.ts
@@ -13,4 +13,14 @@ export async function seed(knex: Knex): Promise<void> {
             booking_rating: chance.integer({ min:1, max:10})
         }))
     ]);
+
+    await knex("staging_users_register").del();
+
+    // Inserts seed entries
+    await knex("staging_users_register").insert([
+        ...Array.from({ length: 20 }, (_, i) => ({ 
+            booking_user_source: chance.integer({ min: 10000, max: 10192 }),
+            booking_user_promotion: chance.weighted(["corp", "sales", "earlyBird", "compensation", "event", "anniversary"], [0.14, 0.18, 0.31, 0.02, 0.16, 0.32])
+        }))
+    ]);
 };
