b3b2d217b88caff52b32250475d42ddbb4ee0924
John Doe <123931543+onu941@users.noreply.github.com>
2023-06-21 15:17:27 +0800

    update logo

--- vite-client/src/components/InitialLanding.tsx
+++ vite-client/src/components/InitialLanding.tsx
@@ -1,11 +1,11 @@
 import { InitialLandingProps } from "../app/interface";
 import { PrimaryButton } from "./minicomponents/Buttons";
-import logo from "../assets/logo04.png";
+import logo from "../assets/logo05.png";
 
 export function InitialLanding(props: InitialLandingProps) {
   return (
-    <div className="flex flex-col place-content-center place-items-center">
-      <div className="md:w-96 w-80 rounded-xl flex place-content-center place-items-center mb-14">
+    <div className="flex flex-col place-content-center place-items-center w-full">
+      <div className="md:w-7/8 rounded-xl flex place-content-center place-items-center mb-14">
         <img src={logo} className="w-96"></img>
       </div>
       <PrimaryButton
