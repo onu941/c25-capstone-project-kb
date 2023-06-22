09988eeae58b7031d894ccda496b72f3ac07f809
John Doe <123931543+onu941@users.noreply.github.com>
2023-06-22 19:05:34 +0800

    fixed

--- nest-server/src/auth/auth.module.ts
+++ nest-server/src/auth/auth.module.ts
@@ -1,19 +1,19 @@
-import { Module, forwardRef } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { UserService } from 'src/user/user.service';
-import { JwtStrategy } from './jwt.strategy';
-import { UserModule } from 'src/user/user.module';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
+import { Module, forwardRef } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { UserService } from "src/user/user.service";
+import { JwtStrategy } from "./jwt.strategy";
+import { UserModule } from "src/user/user.module";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
 
 @Module({
   imports: [
     PassportModule,
     forwardRef(() => UserModule),
     JwtModule.register({
-      secret: 'very_secret_123',
-      signOptions: { expiresIn: '1d' },
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: "1d" },
     }),
   ],
   controllers: [AuthController],
