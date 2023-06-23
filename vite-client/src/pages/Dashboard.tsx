9198734cdcf1d024d423e0620f1f8dd3788b9f79
John Doe <123931543+onu941@users.noreply.github.com>
2023-06-23 18:51:34 +0800

    finalise dashboard and add missing landing links

--- vite-client/src/pages/Dashboard.tsx
+++ vite-client/src/pages/Dashboard.tsx
@@ -6,6 +6,7 @@ import {
 import { AppHeader } from "../components/minicomponents/Headers";
 import { Sidebar } from "../components/minicomponents/Sidebar";
 import { Tab } from "../components/minicomponents/Tab";
+import { PrimaryButton } from "../components/minicomponents/Buttons";
 
 export function Dashboard() {
   // const token = localStorage.getItem("token");
@@ -42,6 +43,20 @@ export function Dashboard() {
             toggleSidebar={toggleSidebar}
           ></Sidebar>
         </ResponsiveContainer>
+        <div className="w-full h-2/3 flex flex-col place-content-center place-items-center flex-wrap text-center text-2xl text-slate-300">
+          <div className="mb-18">
+            Our data anlaysts at Partymate have been looking at your partyrooms'
+            performance!
+          </div>
+          <div className="py-12">
+            <a
+              href="https://app.powerbi.com/groups/me/reports/14fc615e-b91a-445e-84a4-082957fd4998/ReportSection2425e5bc117145b5a020?experience=power-bi"
+              target="_blank"
+            >
+              <PrimaryButton label="Find out more here" />
+            </a>
+          </div>
+        </div>
       </FullScreen>
       <Tab />
     </>
