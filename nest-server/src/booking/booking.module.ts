1af4d2b9f67114469ecd84f425fffa798c8fc614
John Doe <123931543+onu941@users.noreply.github.com>
2023-06-23 17:45:16 +0800

    replace localhost with process.env

--- nest-server/src/booking/booking.module.ts
+++ nest-server/src/booking/booking.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { BookingController } from './booking.controller';
-import { BookingService } from './booking.service';
-import { AuthModule } from 'src/auth/auth.module';
-import { KnexModule } from 'nestjs-knex';
+import { Module } from "@nestjs/common";
+import { BookingController } from "./booking.controller";
+import { BookingService } from "./booking.service";
+import { AuthModule } from "src/auth/auth.module";
+import { KnexModule } from "nestjs-knex";
 
 @Module({
   imports: [
@@ -10,9 +10,9 @@ import { KnexModule } from 'nestjs-knex';
     KnexModule.forRootAsync({
       useFactory: async () => ({
         config: {
-          client: 'pg',
+          client: "pg",
           connection: {
-            host: 'localhost',
+            host: process.env.DB_HOST,
             port: 5432,
             user: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
