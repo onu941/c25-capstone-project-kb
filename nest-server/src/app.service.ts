cf0e70fe0b982936b0666bd4f11d0f0d2b0dc3e5
John Doe <123931543+onu941@users.noreply.github.com>
2023-08-11 13:45:16 +0800

    adjust hello world

--- nest-server/src/app.service.ts
+++ nest-server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Hello World! Running NestJS. Thank you.";
+    return "Hello World! Running NestJS";
   }
 }
