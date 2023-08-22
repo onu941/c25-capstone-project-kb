3ee03137cab38c37aeeb7bd933db1339e32b85a1
John Doe <123931543+onu941@users.noreply.github.com>
2023-08-22 13:43:49 +0800

    testing validation

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -5,10 +5,12 @@ import { multerConfig } from "./multer.config";
 import { MulterModule } from "@nestjs/platform-express";
 import path, { join } from "path";
 import express from "express";
+import { ValidationPipe } from "./validation/validation.pipe";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+  // app.useGlobalPipes(new ValidationPipe());
   app.useStaticAssets(path.resolve("uploads"));
 
   let PORT = 8080;
